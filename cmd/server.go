98f95ce614224a9a8d5f8109a49972129d4487cf
Yury Getman <stagg7777@gmail.com>
2022-01-20 01:24:21 +0300

    implements get/post serving

--- cmd/server.go
+++ cmd/server.go
@@ -1,32 +1,147 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/spf13/cobra"
+
+	"github.com/imblowfish/what-to-farm-test/crypto"
 )
+
+type safeMap struct {
+	mu     sync.Mutex
+	prices map[string]string
+}
+
+func NewSafeMap() *safeMap {
+	return &safeMap{
+		prices: make(map[string]string),
+	}
+}
+
+func (m *safeMap) Set(symbol string, newPrice string) {
+	m.mu.Lock()
+	m.prices[symbol] = newPrice
+	m.mu.Unlock()
+}
+
+func (m *safeMap) Value(symbol string) string {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	price, ok := m.prices[symbol]
+	if !ok {
+		return ""
+	}
+	return price
+}
+
+func (m *safeMap) Get() map[string]string {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	return m.prices
+}
+
+func (m *safeMap) Print() {
+	m.mu.Lock()
+	fmt.Println(m.prices)
+	m.mu.Unlock()
+}
 
 var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Short server description",
 	Long:  "Long server description",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
+		const requestDuration = 5 * time.Second
+		pricesMap := NewSafeMap()
+
 		go func() {
 			for {
-				currenciesData, err := BinanceRequester{}.MakeRequest()
+				cryptosInfo, err := crypto.MakeBinanceRequest()
 				if err != nil {
 					os.Exit(1)
 				}
-				for _, v := range currenciesData {
-					if contains(GetBinanceSideSupportedCryptoName(), v.Symbol) {
-						fmt.Println(v)
+				for _, v := range cryptosInfo {
+					if contains(crypto.GetBinanceSupportedCryptos(), v.Symbol) {
+						// save new price in the prices map
+						pricesMap.Set(v.Symbol, v.Price)
 					}
 				}
+				// pricesMap.Print()
+				time.Sleep(requestDuration)
 			}
 		}()
-		// TODO: Run server
-		time.Sleep(15 * time.Second)
+
+		// run server
+		http.HandleFunc("/api/v1/rates", func(w http.ResponseWriter, req *http.Request) {
+			switch req.Method {
+			case "GET":
+				if err := req.ParseForm(); err != nil {
+					fmt.Println("Cannot parse form")
+				}
+				pairs := strings.Split(strings.Replace(req.Form.Get("pairs"), " ", "", 1), ",")
+				fmt.Println(pairs)
+
+				w.Header().Set("Content-Type", "application/json")
+				resp := make(map[string]string)
+				for _, pair := range pairs {
+					priceValue := pricesMap.Value(crypto.ConvertToBinanceSymbol(pair))
+					if len(priceValue) != 0 {
+						resp[pair] = priceValue
+					}
+				}
+				jsonResp, err := json.Marshal(resp)
+				if err != nil {
+					fmt.Errorf("Cannot create json response %s", err)
+					return
+				}
+				w.Write(jsonResp)
+
+			case "POST":
+				body, err := ioutil.ReadAll(req.Body)
+				if err != nil {
+					fmt.Println("Cannot read body")
+				}
+				fmt.Println(string(body))
+				var currencies map[string][]string
+				if err := json.Unmarshal(body, &currencies); err != nil {
+					fmt.Println("Cannot parse json", err)
+				}
+
+				w.Header().Set("Content-Type", "application/json")
+				resp := make(map[string]string)
+				for _, pair := range currencies["pairs"] {
+					priceValue := pricesMap.Value(crypto.ConvertToBinanceSymbol(pair))
+					if len(priceValue) != 0 {
+						resp[pair] = priceValue
+					}
+				}
+
+				jsonResp, err := json.Marshal(resp)
+				if err != nil {
+					fmt.Errorf("Cannot create json response %s", err)
+					return
+				}
+				w.Write(jsonResp)
+
+			default:
+				http.NotFound(w, req)
+			}
+		})
+
+		fmt.Println("Listening at port 3001...")
+		err := http.ListenAndServe(":3001", nil)
+		if err != nil {
+			return fmt.Errorf("Cannot run server %s", err)
+		}
+
+		return nil
 	},
 }
---
+++ crypto/binance.go
@@ -0,0 +1,62 @@
+package crypto
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type CryptoInfo struct {
+	Symbol string
+	Price  string
+}
+
+func GetBinanceSupportedCryptos() []string {
+	return []string{
+		"ETHUSDT",
+		"BTCUSDT",
+		// TODO @imblowfish: add new cryptos here...
+	}
+}
+
+func ConvertToBinanceSymbol(symbol string) string {
+	converted, ok := map[string]string{
+		"ETH-USDT": "ETHUSDT",
+		"BTC-USDT": "BTCUSDT",
+	}[symbol]
+	if !ok {
+		converted = ""
+	}
+	return converted
+}
+
+func ConvertFromBinanceSymbol(symbol string) string {
+	converted, ok := map[string]string{
+		"ETHUSDT": "ETH-USDT",
+		"BTCUSDT": "BTC-USDT",
+	}[symbol]
+	if !ok {
+		converted = ""
+	}
+	return converted
+}
+
+func MakeBinanceRequest() ([]CryptoInfo, error) {
+	resp, err := http.Get("https://api.binance.com/api/v3/ticker/price")
+	if err != nil {
+		return nil, fmt.Errorf("Cannot exec GET request %s", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot read response body %s", err)
+	}
+	var currenciesData []CryptoInfo
+	if err := json.Unmarshal(body, &currenciesData); err != nil {
+		return nil, fmt.Errorf("Cannot parse json response %s", err)
+	}
+
+	return currenciesData, nil
+}
